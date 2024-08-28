fc62b4d2270610935ac1492f8d3ad46e73ff0c5a
WazedKhan <wazedkhan119399@gmail.com>
2024-08-29 00:15:25 +0600

    feat: using alembic migrations django like

--- apps/restaurant/views/order_views.py
+++ apps/restaurant/views/order_views.py
@@ -4,6 +4,23 @@ from fastapi import FastAPI, BackgroundTasks
 from restaurant.utils.task_manager import simulate_order_placing
 from restaurant.models.task_status import task_status
 
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from ..models import items
+from apps import database
+
+items.Base.metadata.create_all(bind=database.engine)
+
+
+# Dependency to get the database session
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 app = FastAPI()
 
 
@@ -15,6 +32,15 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.post("/food_items/", response_model=items.FoodItem)
+def create_food_item(food_item: items.FoodItem, db: Session = Depends(get_db)):
+    db_food_item = items.FoodItem(**food_item.dict())
+    db.add(db_food_item)
+    db.commit()
+    db.refresh(db_food_item)
+    return db_food_item
 
 
 @app.post("/simulate-order-placing/{order_id}")
